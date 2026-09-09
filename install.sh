036d75bf5461a5971f04b6722e77d90cd83e0883
gabriellglrs <gabriellglrs@users.noreply.github.com>
2026-09-09 19:17:39 -0300

    chore: repo renomeado para tech-hub--dev.gabriellucas

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Instala e restaura o setup Zsh + Powerlevel10k (Ubuntu/WSL2)
 # Uso:
-#   git clone https://github.com/gabriellglrs/tech-hub_dev.gabriellucas.git
-#   cd tech-hub_dev.gabriellucas
+#   git clone https://github.com/gabriellglrs/tech-hub--dev.gabriellucas.git
+#   cd tech-hub--dev.gabriellucas
 #   ./install.sh            # base dev (zsh, plugins, ferramentas dev)
 #   ./install.sh --sec      # base + cyberseg
 #   ./install.sh --copy-only # só copia .zshrc/.p10k sem apt
